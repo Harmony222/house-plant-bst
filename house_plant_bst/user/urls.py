8b1a841c0b07b455943d15673acbd5e99b6f3e1d
harmony222 <scarleth@oregonstate.edu>
2022-01-25 07:31:47 -0800

    add user update profile functionality

--- house_plant_bst/user/urls.py
+++ house_plant_bst/user/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from . import views
-from .views import SignUpView
+from .views import SignUpView, UpdateUserView
+from django.views.generic import TemplateView
 
 app_name = "user"
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('profile/', views.profile, name='profile'),
-    # path('login/', views.login, name='login'),
+    path(
+        'profile/',
+        TemplateView.as_view(template_name='user/profile.html'),
+        name='profile',
+    ),
+    path('update/', UpdateUserView.as_view(), name='update_user'),
 ]
--- house_plant_bst/user/views.py
+++ house_plant_bst/user/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from .forms import CustomUserCreationForm
-
-# Create your views here.
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class UpdateUserView(UpdateView):
+    form_class = CustomUserChangeForm
+    success_url = '/user/profile/'
+    template_name = 'registration/update.html'
+    model = CustomUser
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(self.model, pk=self.request.user.pk)
 
 
 def profile(request):
