b5e1ac7d64e5a84281b80abecb076fccad992bc4
harmony222 <scarleth@oregonstate.edu>
2022-01-26 09:07:21 -0800

    uncomment out models import in plants

--- house_plant_bst/plant/models.py
+++ house_plant_bst/plant/models.py
@@ -1,4 +1,4 @@
-# from django.db import models
+from django.db import models
 
 
 class Plant(models.Model):
@@ -10,19 +10,19 @@ class Plant(models.Model):
         verbose_name_plural = 'Plants'
 
     def __str__(self):
-        """String for representing the Plant object (ex: the Admin site). """
+        """String for representing the Plant object (ex: the Admin site)."""
         return self.scientific_name
 
 
 class PlantCommonName(models.Model):
     name = models.CharField(max_length=150)
-    plant_id = models.ForeignKey(Plant, verbose_name="Plant",
-                                 on_delete=models.CASCADE)
+    plant_id = models.ForeignKey(
+        Plant, verbose_name="Plant", on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name_plural = 'Plant Common Names'
 
     def __str__(self):
-        """String for representing the Plant object (ex: the Admin site). """
+        """String for representing the Plant object (ex: the Admin site)."""
         return self.name
-
