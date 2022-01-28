29575b1bade666ff9bef2d51d405919d1c60e342
harmony222 <scarleth@oregonstate.edu>
2022-01-28 11:47:26 -0800

    small fix for linting check

--- house_plant_bst/plant/views.py
+++ house_plant_bst/plant/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Plant, PlantCommonName
+from .models import Plant
 from .mixins import TemplateTitleMixin
 
 
@@ -9,10 +9,35 @@ class PlantListView(TemplateTitleMixin, ListView):
     model = Plant
     title = 'Plants'
 
+    # def get_queryset(self):
+    #     qs = self.model.objects.all().plantcommonname_set.all()
+    #     print(qs)
+    #     return qs
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = self.get_title()
+        # print(context)
+        for sci_plant in context['object_list']:
+            sci_plant['test'] = 'test'
+            # sci_plant['common_names']
+            # sci_plant['common_name'] = PlantCommonName.objects.filter(
+            #     plant=sci_plant.pk
+            # )
+            print(sci_plant)
+
+        return context
+
 
 class PlantDetailView(TemplateTitleMixin, DetailView):
     model = Plant
     title = 'Plant Detail'
+
+    # def get_queryset(self):
+    #     url_pk = self.kwargs.get('pk')
+    #     qs = self.model.objects.get(pk=url_pk).get_common_names.all()
+    #     print(qs)
+    #     return qs
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
