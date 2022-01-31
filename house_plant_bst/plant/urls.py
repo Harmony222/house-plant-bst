8b6e7ff17433497869f7514604b3155827e9e4e1
harmony222 <scarleth@oregonstate.edu>
2022-01-31 14:22:21 -0800

    added plant update and delete functionality, added corresponding links

--- house_plant_bst/plant/urls.py
+++ house_plant_bst/plant/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
     path('', views.PlantListView.as_view(), name='all_plants'),
     path('<int:pk>/', views.PlantDetailView.as_view(), name='plant_detail'),
     path('create/', views.PlantCreateView.as_view(), name='plant_create'),
+    path(
+        '<int:pk>/update/',
+        views.PlantUpdateView.as_view(),
+        name='plant_update',
+    ),
+    path(
+        '<int:pk>/delete/',
+        views.PlantDeleteView.as_view(),
+        name='plant_delete',
+    ),
 ]
