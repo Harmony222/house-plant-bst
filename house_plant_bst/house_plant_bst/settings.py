c6a4c3c54d6bf0850ca6b5cd3f19a4c02a1ff114
harmony222 <scarleth@oregonstate.edu>
2022-02-02 11:10:58 -0800

    edit to remove key error

--- house_plant_bst/house_plant_bst/settings.py
+++ house_plant_bst/house_plant_bst/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-x^(8bb$mjg++o@h$map*=9-3eisr9ajm0!^$axt)%wc%!2dx0i',
+)
 # SECRET_KEY = (
 #     'django-insecure-x^(8bb$mjg++o@h$map*=9-3eisr9ajm0!^$axt)%wc%!2dx0i'
 # )
