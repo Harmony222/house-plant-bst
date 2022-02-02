3e568f9b5a6ec1a3ecaade92732fc1fa56c5be38
harmony222 <scarleth@oregonstate.edu>
2022-02-02 12:40:37 -0800

    test local db setup #2

--- house_plant_bst/settings.py
+++ house_plant_bst/settings.py
@@ -100,13 +100,14 @@ if DEBUG:
             'PORT': '3306',
         }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
