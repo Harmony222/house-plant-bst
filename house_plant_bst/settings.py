3c59faa4d2d091f24db985fc9f15e995254bd381
harmony222 <scarleth@oregonstate.edu>
2022-03-01 08:55:10 -0800

    modify settings for heroku; adjust order history Order display slightly

--- house_plant_bst/settings.py
+++ house_plant_bst/settings.py
@@ -93,9 +93,9 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD'],
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
             'HOST': '127.0.0.1',
             'PORT': '3306',
         }
