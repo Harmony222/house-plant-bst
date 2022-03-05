eb7070a9314726e764aba64f2acf5faf7a29f8b3
Jennifer Zhang <zhangjen@oregonstate.edu>
2022-03-05 03:24:36 -0800

    changed Trade model to include both requester and seller's address

--- trade/views.py
+++ trade/views.py
@@ -51,16 +51,16 @@ class CreateTrade(View):
                     handling_methods = ['shipping_choice'] if \
                         seller_plant.is_for_shipping else ['pickup_choice']
                 if 'addresses' in form.cleaned_data.keys():
-                    address = form.cleaned_data['addresses']
+                    requester_address = form.cleaned_data['addresses']
                 else:
-                    address = None
+                    requseter_address = None
                 new_trade_attributes = {
                     'seller_plant': form.cleaned_data['seller_plant'],
                     'seller': seller,
                     'buyer': request.user,
                     'buyer_plants': form.cleaned_data['user_plants_for_trade'],
                     'handling_methods': handling_methods,
-                    'address': address
+                    'requester_address': requester_address
                 }
                 try:
                     existing_trade_id = _trade_exists(new_trade_attributes)
@@ -278,7 +278,7 @@ def _create_new_trade_and_items(new_trade_attr_dict):
     # seller didn't specify, buyer can offer shipping and/or pickup
     else:
         accepted_handling_method = 'UN'
-    address = new_trade_attr_dict['address']
+    requester_address = new_trade_attr_dict['requester_address']
 
     # add buyer plants if still available
     buyer_plants = _trade_plants_are_available(seller_plant, buyer_plants)
@@ -291,7 +291,7 @@ def _create_new_trade_and_items(new_trade_attr_dict):
         accepted_handling_method=accepted_handling_method,
         is_offered_for_shipping=is_offered_for_shipping,
         is_offered_for_pickup=is_offered_for_pickup,
-        address=address
+        requester_address=requester_address
     )
     new_trade.save()
 
